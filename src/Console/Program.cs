44488995aecbc19b459c2bf3af1fb4f696f27c07
Rafael Berto <5by5.rberto@voeazul.com.br>
2023-12-07 11:13:42 -0300

    implementacao do codigo

--- src/Console/Program.cs
+++ src/Console/Program.cs
@@ -1,2 +1,54 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+
 Console.WriteLine("Hello, World!");
+
+try
+{
+    QuestPDF.Settings.License = LicenseType.Community;
+
+    Document.Create(container =>
+    {
+        container.Page(page =>
+        {
+            page.Size(PageSizes.A4);
+            page.Margin(2, Unit.Centimetre);
+            page.PageColor(Colors.White);
+            page.DefaultTextStyle(x => x.FontSize(20));
+
+            page.Header()
+                .Text("Pdg Generator")
+                .SemiBold()
+                .FontSize(40);
+
+            page.Content()
+            .PaddingVertical(1, Unit.Centimetre)
+            .Column(x =>
+            {
+                x.Item()
+                    .Text(Placeholders.LoremIpsum());
+
+                x.Item()
+                    .Image(Placeholders.Image(200, 100));
+
+            });
+
+            page.Footer()
+                .AlignCenter()
+                .Text(x =>
+                {
+                    x.Span("Page ");
+                    x.CurrentPageNumber();
+                });
+        });
+    }).GeneratePdf("generatorpdf.pdf");
+
+    Console.WriteLine("Pdf generated");
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Falha ao gerar Pdf: {ex.Message}");
+}
